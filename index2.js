8866f5dfd045d40415588091c06ae06f7a3e6df9
bbalino000 <balinobryan@gmail.com>
2024-11-12 16:04:09 +0800

    Changes

--- index2.js
+++ index2.js
@@ -1,48 +1,60 @@
-const LIFF_ID_TEST1 = "2006459403-LdVrj6jz";
 const LIFF_ID_TEST2 = "2006459403-zRa7323K";
 
-document.addEventListener("DOMContentLoaded", function() {
-  let isInClient = liff.isInClient();
-  $('#isInCLient').text(isInClient);
+// document.addEventListener("DOMContentLoaded", function() {
+//   let isInClient = liff.isInClient();
+//   $('#isInCLient').text(isInClient);
 
-  liff.init({ liffId: LIFF_ID_TEST2 }, successCallback, errorCallback);
+//   liff.init({ liffId: LIFF_ID_TEST2 }, successCallback, errorCallback);
 
-  // if (!liff.isLoggedIn()) {
-  //   lineGetProfile();
-  // }
+//   // if (!liff.isLoggedIn()) {
+//   //   lineGetProfile();
+//   // }
     
-  $('#btn-line-samepage').click(() => {
-    liff.login();
-  });
+//   $('#btn-line-samepage').click(() => {
+//     liff.login();
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+  liff.init({ liffId: LIFF_ID_TEST2 })
+    .then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        lineGetProfile();
+      }
+    })
+    .catch(err => console.error('LIFF Initialization Failed', err));
+  
+  function lineLogout() {
+    liff.logout();
+    window.location.reload();
+  }
+  
+  function lineGetProfile() {
+    liff.getProfile().then(profile => {
+      let isLoggedIn = liff.isLoggedIn();
+      let accessToken = liff.getAccessToken();
+      let userId = profile.userId;
+  
+      $("#isLoggedIn").text(isLoggedIn);
+      $("#accessToken").val(accessToken);
+      $("#lineUid").val(userId);
+    }).catch((err) => {
+      console.log('lineGetProfile Error', err);
+    });
+  }
 });
 
-function successCallback() {
-  console.log('LIFF successCallback');
-  if(liff.isLoggedIn()) {
-    lineGetProfile();
-  }
-}
+// function successCallback() {
+//   console.log('LIFF successCallback');
+//   if(liff.isLoggedIn()) {
+//     lineGetProfile();
+//   }
+// }
 
-function errorCallback() {
-  console.log('LIFF errorCallback');
-  console.log('Error Callback...');
-}
+// function errorCallback() {
+//   console.log('LIFF errorCallback');
+//   console.log('Error Callback...');
+// }
 
-function lineLogout() {
-  liff.logout();
-  location.reload();
-}
-
-function lineGetProfile() {
-  liff.getProfile().then(profile => {
-    let isLoggedIn = liff.isLoggedIn();
-    let accessToken = liff.getAccessToken();
-    let userId = profile.userId;
-
-    $("#isLoggedIn").text(isLoggedIn);
-    $("#accessToken").val(accessToken);
-    $("#lineUid").val(userId);
-  }).catch((err) => {
-    console.log('Profile', err);
-  });
-}
